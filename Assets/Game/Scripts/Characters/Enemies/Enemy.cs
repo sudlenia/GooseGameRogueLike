2622f7396f3f3c8f3b54480556127d63a581e22e
ilyarr <ilya-rageishes@yandex.ru>
2023-12-22 00:34:29 +0700

    гусь краснеет при атаке

--- Assets/Game/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Game/Scripts/Characters/Enemies/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class Enemy : Entity
@@ -57,6 +58,14 @@ public abstract class Enemy : Entity
         if (health <= 0) Die();
     }
 
+    private IEnumerator GooseOnAttack(Collider2D goose)
+    {
+        SpriteRenderer sr = goose.GetComponent<SpriteRenderer>();
+        sr.color = new Color(1f, 0.5f, 0.5f);
+        yield return new WaitForSeconds(0.2f);
+        sr.color = new Color(1, 1, 1);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -65,6 +74,7 @@ public abstract class Enemy : Entity
             if (goose != null)
             {
                 goose.GetDamage(damage + (damage * damageIncrease));
+                StartCoroutine(GooseOnAttack(collision));
             }
         }
     }
